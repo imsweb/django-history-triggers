3266149bda68447f78abf5d22b3fc2d79eeb3957
Dan Watson <dcwatson@gmail.com>
2022-01-03 12:03:00 -0500

    Use README as long_description

--- history/__init__.py
+++ history/__init__.py
@@ -1,5 +1,5 @@
 from .backends import get_backend  # noqa
 from .utils import get_history_model, get_request_context  # noqa
 
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 __version_info__ = tuple(int(num) for num in __version__.split(".") if num.isdigit())
--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import re
 
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
 with open(os.path.join("history", "__init__.py"), "r") as src:
     version = re.match(r'.*__version__ = "(.*?)"', src.read(), re.S).group(1)
 
@@ -10,6 +13,8 @@ setup(
     name="django-history-triggers",
     version=version,
     description="Management command and middleware for Django history triggers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Dan Watson",
     author_email="watsond@imsweb.com",
     url="https://github.com/imsweb/django-history-triggers",
