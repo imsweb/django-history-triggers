a0c58f9b77eabcd67946a8c29b60b3389e210d60
ruttenb <ruttenb@imsweb.com>
2024-03-04 11:10:05 -0500

    run black

--- history/conf.py
+++ history/conf.py
@@ -33,5 +33,7 @@ REQUEST_USER_ATTRIBUTE = getattr(settings, "HISTORY_REQUEST_USER_ATTRIBUTE", "pk
 
 # A list of path prefixes the history middleware should ignore.
 MIDDLEWARE_IGNORE = getattr(
-    settings, "HISTORY_MIDDLEWARE_IGNORE", [settings.STATIC_URL.lstrip('/'), settings.MEDIA_URL.lstrip('/')]
+    settings,
+    "HISTORY_MIDDLEWARE_IGNORE",
+    [settings.STATIC_URL.lstrip("/"), settings.MEDIA_URL.lstrip("/")],
 )
